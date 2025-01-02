d4d47b8e1f443ac1e87e00a9d8b23a5b7b17e05e
ichanddan <code.us.cm@gmail.com>
2025-01-02 22:35:40 +0530

    Enhance Login component with form handling and integrate RNE UI components; update App navigation to set initial route and add new dependencies for RNE UI

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Signup" component={Signup} />
         <Stack.Screen name="Login" component={Login} />
--- Components/Pages/Login.js
+++ Components/Pages/Login.js
@@ -1,12 +1,69 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, Alert } from "react-native";
+import React, { useState } from "react";
+import { Button, Input } from "@rneui/base";
 
 const Login = () => {
+    const [form, setForm] = useState({
+        email: "",
+        password: "",
+    })
+    const handleChange = (key, value) => {
+        setForm({...form, [key]: value})
+    }
+    const signInWithEmail = () => {
+        console.log(form)
+        Alert/*  */.alert("Sign in with email")
+    }
+    const signUpWithEmail = () => {
+        console.log(form)
+    }
   return (
-    <View>
-      <Text>Login</Text>
+    <View style={styles.container}>
+      <View style={[styles.verticallySpaced, styles.mt20]}>
+        <Input
+          label="Email"
+          leftIcon={{ type: "font-awesome", name: "envelope" }}
+          placeholder="email@address.com"
+          autoCapitalize={"none"}
+          onChangeText={(value) => handleChange("email", value)}
+        />
+      </View>
+      <View style={styles.verticallySpaced}>
+        <Input
+          label="Password"
+          leftIcon={{ type: "font-awesome", name: "lock" }}
+          secureTextEntry={true}
+          placeholder="Password"
+          autoCapitalize={"none"}
+          onChangeText={(value) => handleChange("password", value)}
+        />
+      </View>
+      <View style={[styles.verticallySpaced, styles.mt20]}>
+        <Button title="Sign in" onPress={() => signInWithEmail(form)} />
+      </View>
+      <View style={styles.verticallySpaced}>
+        <Button title="Sign up" onPress={() => signUpWithEmail()} />
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    textAlign: "center",
+  },
+  verticallySpaced: {
+    paddingTop: 4,
+    paddingBottom: 4,
+    width: "100%",
+    alignSelf: "stretch",
+  },
+  mt20: {
+    marginTop: 20,
+  },
+});
