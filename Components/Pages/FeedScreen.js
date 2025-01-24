2ef8f420f034778af46f87eedb3a8e1a17b60a7c
ichanddan <code.us.cm@gmail.com>
2025-01-24 23:26:00 +0530

    Refactor FeedScreen component for improved code readability and consistency. Updated import statements to use double quotes, formatted JSX for better clarity, and ensured consistent styling properties.

--- Components/Pages/FeedScreen.js
+++ Components/Pages/FeedScreen.js
@@ -1,29 +1,28 @@
-import { View, Text, StyleSheet } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
 
-const FeedScreen = ({navigation}) => {
-  setTimeout(()=>{
-    navigation.navigate('OpeningScreen')
-  }, 2000)
+const FeedScreen = ({ navigation }) => {
+  setTimeout(() => {
+    navigation.navigate("OpeningScreen");
+  }, 2000);
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Loading...</Text>
     </View>
-  )
-}
+  );
+};
 
-export default FeedScreen
+export default FeedScreen;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center"
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 20
   }
-
-})
+});
